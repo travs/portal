0a1ffcd679f3aee7aa818e2aa6d8da94c410a261
retotrinkler <reto.trinkler@gmail.com>
2016-12-21 05:30:02 +0100

    fund wallet button

--- imports/ui/components/wallet/wallet_list.js
+++ imports/ui/components/wallet/wallet_list.js
@@ -25,11 +25,8 @@ Template.wallet_list.helpers({
   wallets() {
     return Session.get('clientAccountList');
   },
-  fromWei(weiValue, type) {
-    return web3.fromWei(weiValue, type).toString(10);
-  },
   displayBalance(balance) {
-    return EthTools.formatBalance(balance.toString(10), '0,0.0[00] UNIT');
+    return EthTools.formatBalance(balance, '0,0.0[00] UNIT');
   },
 });
 
@@ -45,88 +42,54 @@ Template.wallet_list.onRendered(function walletListOnRendered() {
 
 
 Template.wallet_list.events({
-  // 'click .export'() {
-  //   $('#modalExport').openModal();
-  //   // Init Reactive Dict
-  //   const reactiveState = Template.instance().state;
-  //
-  //   if (WalletInstance.currentAddress() === false)
-  //     return;
-  //
-  //   // let password = prompt('Enter password to show your seed. Do not let anyone else see your seed.', 'password');
-  //   const password = 'password';
-  //   lightwallet.keystore.deriveKeyFromPassword(password, function(err, pwDerivedKey) {
-  //     const seed = WalletInstance.export(pwDerivedKey);
-  //     // Set value to form element
-  //     reactiveState.set({ seed });
-  //   });
-  // },
-  // 'click .unlock'() {
-  //   // Get Wallet document
-  //   const seed = this.seed;
-  //   const address = this.address;
-  //   const doc = Wallets.findOne({ address });
-  //   const walletId = doc._id;
-  //
-  //   // Close open Wallet
-  //   if (Wallets.find({ address, open: true }).count() !== 0) {
-  //     WalletInstance.clear();
-  //     Meteor.call('wallets.updateStatus', walletId, false);
-  //     Materialize.toast('Wallet closed', 4000, 'green');
-  //     return;
-  //   }
-  //
-  //   // Can only unlock one Wallet at a time
-  //   if (Wallets.find({ open: true }).count() !== 0) {
-  //     Materialize.toast('Can not unlock more than one wallet at a time', 4000, 'orange');
-  //     return;
-  //   }
-  //
-  //   // Unlock Wallet
-  //   // let password = prompt('Enter Password to encrypt your seed', 'password');
-  //   const password = 'password';
-  //   lightwallet.keystore.deriveKeyFromPassword(password, function(err, pwDerivedKey) {
-  //     WalletInstance.import(seed, pwDerivedKey);
-  //     Meteor.call('wallets.updateStatus', walletId, true);
-  //   });
-  // },
-  // 'click .delete'() {
-  //   $('#modalDelete').openModal();
-  //   // Init Reactive Dict
-  //   const reactiveState = Template.instance().state;
-  //   const walletId = this._id;
-  //   reactiveState.set({ walletId });
-  // },
-  // 'click .deleteAgreed'() {
-  //   // Init Reactive Dict
-  //   const reactiveState = Template.instance().state;
-  //   const walletId = reactiveState.get('walletId');
-  //   // Delete
-  //   WalletInstance.clear();
-  //   Meteor.call('wallets.remove', walletId);
-  //   reactiveState.set({ walletId: '' });
-  //   Materialize.toast('Wallet deleted!', 4000, 'green')
-  // },
-  // 'click .lock'() {
-  //   // Get Wallet document
-  //   const seed = this.seed;
-  //   const address = this.address;
-  //   const doc = Wallets.findOne({ address });
-  //   const walletId = doc._id;
-  //
-  //   // Close open Wallet
-  //   if (Wallets.find({ address, open: true }).count() !== 0) {
-  //     WalletInstance.clear();
-  //     Meteor.call('wallets.updateStatus', walletId, false);
-  //     Materialize.toast('Wallet closed', 4000, 'green');
-  //     return;
-  //   }
-  // },
-  // 'click .refresh-wallets'() {
-  //   // Prevent default browser form submit
-  //   event.preventDefault();
-  //
-  //   // Refresh all wallets
-  //   WalletInstance.refreshAllWallets();
-  // },
+  'click .fund_wallet'() {
+    const address = Session.get('clientDefaultAccount');
+    Meteor.call('sendTestnetEther', address, (err) => {
+      if(!err) {
+        Materialize.toast('We\'ve sent you some funds', 30000, 'green');
+      } else {
+        console.log(err);
+      }
+    });
+
+    // Wallet listen
+    const balance = Session.get('clientDefaultAccountBalance');
+    var filter = web3.eth.filter('latest').watch(() => {
+      let currBalance;
+      web3.eth.getBalance(address, (error, result) => {
+        if(!error) {
+          currBalance = result.toNumber();
+        } else {
+          Session.set('clientDefaultAccountBalance', undefined);
+        }
+      });
+      // Check if Balance has changed
+      if (currBalance !== balance) {
+        if (currBalance === 0) {
+          Materialize.toast('Congratulations! Your Account has been funded', 6000, 'green');
+          Materialize.toast('Go ahead and create a portfolio now', 4000, 'blue');
+        } else {
+          Materialize.toast('Balance has changed', 4000, 'orange');
+        }
+        // Uninstall Filter
+        filter.stopWatching();
+      }
+    });
+  },
+  'click .refresh-wallets'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Refresh all wallets
+    web3.eth.getBalance(web3.eth.defaultAccount, (error, result) => {
+      if (!error) {
+        Session.set('clientDefaultAccountBalance', result.toNumber());
+      } else {
+        Session.set('clientDefaultAccountBalance', undefined);
+      }
+    });
+
+    // Notification
+    Materialize.toast('Wallets refreshed', 4000, 'blue');
+  },
 });
