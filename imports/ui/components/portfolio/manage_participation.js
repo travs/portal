b36dad649ab39b76b0c9c77bf788f0e4f6889f0c
retotrinkler <reto.trinkler@gmail.com>
2017-01-31 16:01:01 +0100

    working invest && redeem

--- imports/ui/components/portfolio/manage_participation.js
+++ imports/ui/components/portfolio/manage_participation.js
@@ -14,7 +14,9 @@ import './manage_participation.html';
 
 
 Template.manage_participation.onCreated(() => {
+  // TODO update coreContracts param
   Meteor.subscribe('coreContracts');
+
   Template.instance().typeValue = new ReactiveVar(0);
   // Creation of contract object
   Core.setProvider(web3.currentProvider);
@@ -27,7 +29,15 @@ Template.manage_participation.helpers({
     const doc = CoreContracts.findOne({ address });
     return (doc === undefined || address === undefined) ? '' : doc;
   },
-  isInvestingSelected() {
+  formattedSharePrice() {
+    const address = FlowRouter.getParam('address');
+    const doc = CoreContracts.findOne({ address });
+    if (doc !== undefined) {
+      return web3.fromWei(doc.sharePrice, 'ether');
+    }
+    return '';
+  },
+  selectedTypeName() {
     switch (Template.instance().typeValue.get()) {
       case 0: return 'Invest';
       case 1: return 'Redeem';
@@ -44,22 +54,25 @@ Template.manage_participation.onRendered(() => {
 
 Template.manage_participation.events({
   'change select#type': (event, templateInstance) => {
-    const currentlySelectedTypeValue = parseInt(templateInstance.find('select#type').value, 10);
+    const currentlySelectedTypeValue = parseFloat(templateInstance.find('select#type').value, 10);
     Template.instance().typeValue.set(currentlySelectedTypeValue);
   },
   'input input#price': (event, templateInstance) => {
-    const price = parseInt(templateInstance.find('input#price').value, 10);
-    // TODO
+    const price = parseFloat(templateInstance.find('input#price').value, 10);
+    const volume = parseFloat(templateInstance.find('input#volume').value, 10);
+    const total = parseFloat(templateInstance.find('input#total').value, 10);
+    if (!isNaN(volume)) templateInstance.find('input#total').value = price * volume;
+    else if (!isNaN(total)) templateInstance.find('input#volume').value = total / price;
   },
   'input input#volume': (event, templateInstance) => {
-    const price = parseInt(templateInstance.find('input#price').value, 10);
-    const volume = parseInt(templateInstance.find('input#volume').value, 10);
+    const price = parseFloat(templateInstance.find('input#price').value, 10);
+    const volume = parseFloat(templateInstance.find('input#volume').value, 10);
     /* eslint no-param-reassign: ["error", { "props": false }]*/
     templateInstance.find('input#total').value = price * volume;
   },
   'input input#total': (event, templateInstance) => {
-    const price = parseInt(templateInstance.find('input#price').value, 10);
-    const total = parseInt(templateInstance.find('input#total').value, 10);
+    const price = parseFloat(templateInstance.find('input#price').value, 10);
+    const total = parseFloat(templateInstance.find('input#total').value, 10);
     /* eslint no-param-reassign: ["error", { "props": false }]*/
     templateInstance.find('input#volume').value = total / price;
   },
@@ -68,41 +81,68 @@ Template.manage_participation.events({
     event.preventDefault();
 
     // Get value from template instance
-    const type = templateInstance.find('select#type').value;
-    const price = templateInstance.find('input#price').value;
-    const volume = templateInstance.find('input#volume').value;
-    if (!volume || !price) {
+    const type = parseFloat(templateInstance.find('select#type').value, 10);
+    const price = parseFloat(templateInstance.find('input#price').value, 10);
+    const volume = parseFloat(templateInstance.find('input#volume').value, 10);
+    const total = parseFloat(templateInstance.find('input#total').value, 10);
+    if (isNaN(type) || isNaN(price) || isNaN(volume) || isNaN(total)) {
       Materialize.toast('Please fill out the form', 4000, 'blue');
+      return;
     }
+
+    // Init
+    const managerAddress = Session.get('clientMangerAccount');
+    const doc = CoreContracts.findOne({ managerAddress });
+    if (doc === undefined) { Materialize.toast('Undefined document', 4000, 'red'); return; }
+    const coreAddress = doc.address;
+    const coreContract = Core.at(coreAddress);
 
     // Is mining
     Session.set('NetworkStatus', { isInactive: false, isMining: true, isError: false, isMined: false });
 
-    // Init
-
-    // const doc = CoreContracts.findOne({ managerAddress: Session.get('clientMangerAccount') });
-    // const coreContract = (doc === undefined || Session.get('clientMangerAccount') === undefined) ? undefined : Core.at(doc.address);
-    //
-    // // coreContract.sharePrice().then((result) => {
-    // //   console.log(result.toNumber())
-    // // })
-    //
-    // console.log(Template.instance().find('input#volume'));
-    const managerAddress = Session.get('clientMangerAccount');
-    const weiAmount = web3.toWei(volume, 'ether');
-
     // From price to volume of shares
-    const weiShareAmount = web3.toWei(volume * price, 'ether');
+    const weiPrice = web3.toWei(price, 'ether');
+    const weiVolume = web3.toWei(volume, 'ether');
+    const weiTotal = web3.toWei(total, 'ether');
 
     // Invest or Redeem
-
-    if (type === '0') {
-      coreContract.createShares(weiShareAmount, { from: managerAddress, value: weiAmount })
-      .then((result) => {
+    // TODO use switch as above
+    if (type === 0) {
+      coreContract.createShares(weiTotal, { value: weiVolume, from: managerAddress }).then((result) => {
         Session.set('NetworkStatus', { isInactive: false, isMining: false, isError: false, isMined: true });
-        // TODO insert tx w ${result} as txhash
+        // TODO insert txHash into appropriate collection
+        console.log(`Tx Hash: ${result}`);
+        // TODO update coreContracts collection
+        Meteor.call('assets.sync', coreAddress); // Upsert Asset Collection
         return coreContract.totalSupply();
       })
+      .then((result) => {
+        // Update Portfolio collection
+        Meteor.call('coreContracts.setNotional',
+          doc._id,
+          result.toNumber()
+        );
+        return coreContract.calcSharePrice();
+      })
+      // TODO clean up
+      .then((result) => {
+        // Update Portfolio collection
+        console.log(`Shareprice as from contract: ${result.toNumber()}`)
+        Meteor.call('coreContracts.setSharePrice',
+          doc._id,
+          result.toNumber()
+        );
+      });
+    } else if (type === 1) {
+      coreContract.annihilateShares(weiTotal, weiVolume, { from: managerAddress }).then((result) => {
+        Session.set('NetworkStatus', { isInactive: false, isMining: false, isError: false, isMined: true });
+        // TODO insert txHash into appropriate collection
+        console.log(`Tx Hash: ${result}`);
+        // TODO update coreContracts collection
+        Meteor.call('assets.sync', coreAddress); // Upsert Asset Collection
+        return coreContract.totalSupply();
+      })
+      // TODO clean up
       .then((result) => {
         // Update Portfolio collection
         Meteor.call('coreContracts.setNotional',
@@ -114,36 +154,6 @@ Template.manage_participation.events({
       .then((result) => {
         // Update Portfolio collection
         console.log(`Shareprice as from contract: ${result.toNumber()}`)
-        Meteor.call('coreContracts.setSharePrice',
-          doc._id,
-          result.toNumber()
-        );
-      });
-    } else if (type === '1') {
-      console.log(weiShareAmount)
-      const roundingError = 0.01;
-
-      coreContract.calcSharePrice()
-      .then((result) => {
-        console.log(`price: ${result.toString()}`)
-        console.log(weiShareAmount * result.toString() / constants.ether * (1.0 - roundingError))
-        return coreContract.annihilateShares(weiShareAmount, weiShareAmount * result.toString() / SolKeywords.ether * (1.0 - roundingError), {from: managerAddress });
-      })
-      .then((result) => {
-        Session.set('NetworkStatus', { isInactive: false, isMining: false, isError: false, isMined: true });
-        //TODO insert tx w ${result} as txhash
-        return coreContract.totalSupply();
-      })
-      .then((result) => {
-        // Update Portfolio collection
-        Meteor.call('coreContracts.setNotional',
-          doc._id,
-          result.toNumber()
-        );
-        return coreContract.calcSharePrice();
-      })
-      .then((result) => {
-        // Update Portfolio collection
         Meteor.call('coreContracts.setSharePrice',
           doc._id,
           result.toNumber()
