176891bd138fb55b69afb398461c4cc9356669ac
Ahmad S. Afandi <peterdraw@Ahmads-MacBook-Pro.local>
2017-02-05 09:48:59 +0700

    portal design

---
+++ public/portal-design/src/scripts/scripts.js
@@ -0,0 +1,9 @@
+//= require ../components/jquery/dist/jquery.js
+//= require ../../node_modules/d3/build/d3.min.js
+//= require ../components/datatables.net/js/jquery.dataTables.min.js
+//= require ../components/datatables.net-bs/js/dataTables.bootstrap.min.js
+//= require ../components/bootstrap-sass/assets/javascripts/bootstrap.js
+//= require ../components/owl.carousel/dist/owl.carousel.min.js
+//= require ../components/bootstrap-select-sass/js/bootstrap-select.js
+//= require vendor/metricsgraphics/metricsgraphics.min.js
+//= require vendor/bootstrap-switch-master/bootstrap-switch.min.js
