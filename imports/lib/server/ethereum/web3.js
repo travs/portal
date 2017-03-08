0f9c68cd44dc4f3dd574adf112776d56ddd2936e
retotrinkler <reto.trinkler@gmail.com>
2017-03-08 19:12:54 +0000

    Kovan port 8547

--- imports/lib/server/ethereum/web3.js
+++ imports/lib/server/ethereum/web3.js
@@ -1,8 +1,8 @@
 import Web3 from 'web3';
 
 // set the provider you want from Web3.providers
-if ((new Web3(new Web3.providers.HttpProvider('http://localhost:8546'))).isConnected()) {
-  web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8546'));
+if ((new Web3(new Web3.providers.HttpProvider('http://localhost:8547'))).isConnected()) {
+  web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8547'));
 } else {
   web3 = new Web3(new Web3.providers.HttpProvider('http://172.17.0.1:8545'));
 }
