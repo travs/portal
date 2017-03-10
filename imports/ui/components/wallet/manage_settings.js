628190847ca231e03fe6c8f983f2a3f70486a0cf
retotrinkler <reto.trinkler@gmail.com>
2017-03-10 12:03:59 +0000

    bootstrap modal fix

--- imports/ui/components/wallet/manage_settings.js
+++ imports/ui/components/wallet/manage_settings.js
@@ -13,16 +13,17 @@ Template.manage_settings.onCreated(() => {});
 
 Template.manage_settings.helpers({});
 
+
 Template.manage_settings.onRendered(() => {
   $('select').select2();
-
-  $('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').focus()
-  })
 });
 
 
 Template.manage_settings.events({
+  'shown.bs.modal #myModal': (event, templateInstance) => {
+    // Prevent default browser form submit
+    event.preventDefault();
+  },
   'click .manage': (event, templateInstance) => {
     // Prevent default browser form submit
     event.preventDefault();
