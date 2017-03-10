b4ef1528208bbbd20ddeccf7fabac09cf7c7f7f1
Peterdraw <afandihore@gmail.com>
2017-03-10 18:32:02 +0700

    impelementing modal

--- imports/ui/components/wallet/manage_settings.js
+++ imports/ui/components/wallet/manage_settings.js
@@ -3,6 +3,7 @@ import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Session } from 'meteor/session';
 import select2 from 'select2';
+import { $ } from 'meteor/jquery';
 
 import './manage_settings.html';
 
@@ -10,11 +11,14 @@ import './manage_settings.html';
 Template.manage_settings.onCreated(() => {});
 
 
-Template.manage_settings.helpers({
-});
+Template.manage_settings.helpers({});
 
 Template.manage_settings.onRendered(() => {
   $('select').select2();
+
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+  })
 });
 
 
