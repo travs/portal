7319eb2619f785055b2a8c98fd0f1980f07ce04d
Peterdraw <afandihore@gmail.com>
2017-04-06 18:37:08 +0700

    revert back web3 url

--- imports/lib/server/ethereum/web3.js
+++ imports/lib/server/ethereum/web3.js
@@ -4,7 +4,7 @@ import Web3 from 'web3';
 if ((new Web3(new Web3.providers.HttpProvider('http://localhost:8547'))).isConnected()) {
   web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8547'));
 } else {
-  web3 = new Web3(new Web3.providers.HttpProvider('aaa'));
+  web3 = new Web3(new Web3.providers.HttpProvider('https://kovan.melonport.com'));
 }
 
 export default web3;
