ed78c2c0fb6b62b4d4661be3be0ed23c871815b2
retotrinkler <rt@melonport.com>
2017-04-15 21:56:08 +0800

    Kovan Address List

---
+++ imports/lib/ethereum/address_list.js
@@ -0,0 +1,16 @@
+// Assets
+
+exports.EtherToken = 0x7506c7BfED179254265d443856eF9bda19221cD7;
+exports.MelonToken = 0x4dffea52b0b4b48c71385ae25de41ce6ad0dd5a7;
+exports.BitcoinToken = 0x9E4C56a633DD64a2662bdfA69dE4FDE33Ce01bdd;
+exports.EuroToken = 0xC151b622fDeD233111155Ec273BFAf2882f13703;
+exports.RepToken = 0xF61b8003637E5D5dbB9ca8d799AB54E5082CbdBc;
+
+// Price Feeds
+
+exports.Oraclize
+exports.CryptoCompare = 0x442Fd95C32162F914364C5fEFf27A0Dc05214706;
+
+// Exchanges
+
+exports.Exchange = 0x7c7B4904e37C71845FDC1c75D20FF6ee3c80eCB4;
