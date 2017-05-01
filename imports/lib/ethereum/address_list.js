a1e9eeec209462d1a01e774d1867e7dc2bf37cd0
retotrinkler <rt@melonport.com>
2017-05-01 17:02:44 +0200

    Addresses via AddressList in Client.contract.js

--- imports/lib/ethereum/address_list.js
+++ imports/lib/ethereum/address_list.js
@@ -1,39 +1,39 @@
 // TODO replace with actual address
 // Assets
 
-exports.EtherToken = 0x7506c7BfED179254265d443856eF9bda19221cD7;
-exports.MelonToken = 0x4dffea52b0b4b48c71385ae25de41ce6ad0dd5a7;
-exports.BitcoinToken = 0x9E4C56a633DD64a2662bdfA69dE4FDE33Ce01bdd;
-exports.EuroToken = 0xC151b622fDeD233111155Ec273BFAf2882f13703;
-exports.RepToken = 0xF61b8003637E5D5dbB9ca8d799AB54E5082CbdBc;
+exports.EtherToken = '0x7506c7BfED179254265d443856eF9bda19221cD7';
+exports.MelonToken = '0x4dffea52b0b4b48c71385ae25de41ce6ad0dd5a7';
+exports.BitcoinToken = '0x9E4C56a633DD64a2662bdfA69dE4FDE33Ce01bdd';
+exports.EuroToken = '0xC151b622fDeD233111155Ec273BFAf2882f13703';
+exports.RepToken = '0xF61b8003637E5D5dbB9ca8d799AB54E5082CbdBc';
 
 // Price Feeds
 
-exports.CryptoCompare = 0x442Fd95C32162F914364C5fEFf27A0Dc05214706 ;
+exports.CryptoCompare = '0x442Fd95C32162F914364C5fEFf27A0Dc05214706';
 
 // Exchanges
 
-exports.Exchange = 0x1b468875d2b8f2d2c56981db446fbb911b1b16e3;
+exports.Exchange = '0x1b468875d2b8f2d2c56981db446fbb911b1b16e3';
 
 // Universe
 
-exports.Universe = 0x39ed14bf94f23a808f31208bef9c0fdcaa37b740;
+exports.Universe = '0x39ed14bf94f23a808f31208bef9c0fdcaa37b740';
 
 // Participation
 
-exports.Subscribe = 0x0;
-exports.Redeem = 0x0;
+exports.Subscribe = '0x0';
+exports.Redeem = '0x0';
 
 // Risk Mgmt
 
-exports.RiskMgmt = 0x39ed14bf94f23a808f31208bef9c0fdcaa37b740;
+exports.RiskMgmt = '0x39ed14bf94f23a808f31208bef9c0fdcaa37b740';
 
 // Fees
 
-exports.ManagementFee = 0x0;
-exports.PerformanceFee = 0x0;
+exports.ManagementFee = '0x0';
+exports.PerformanceFee = '0x0';
 
 // Governance
 
-exports.Version = 0x0bc659fD55E099e8A2Ed04f2382D994BE011746A;
-exports.Governance = 0x4516acbcf84804af1bf629eced55c894efb2cce3;
+exports.Version = '0x0bc659fD55E099e8A2Ed04f2382D994BE011746A';
+exports.Governance = '0x4516acbcf84804af1bf629eced55c894efb2cce3';
--- imports/startup/client/contracts.js
+++ imports/startup/client/contracts.js
@@ -5,6 +5,7 @@ import { Session } from 'meteor/session';
 import { Cores } from '/imports/api/cores';
 import { Universes } from '/imports/api/modules';
 
+import AddressList from '/imports/lib/ethereum/address_list.js'
 import EtherToken from '/imports/lib/assets/contracts/EtherToken.json';
 import BitcoinToken from '/imports/lib/assets/contracts/BitcoinToken.json';
 import RepToken from '/imports/lib/assets/contracts/RepToken.json';
@@ -16,16 +17,16 @@ import Version from '/imports/lib/assets/contracts/Version.json';
 import Governance from '/imports/lib/assets/contracts/Governance.json';
 
 Meteor.startup(() => {
-  Session.set('etherTokenContractAddress', '0xc5f550c78db2ee33e5867c432e175cac89073772');
-  Session.set('melonTokenContractAddress', '0xfcf98c25129ba729e1822e56ffbd3e758b81ce7c');
-  Session.set('bitcoinTokenContractAddress', '0x23bb1f93c168a290f0626ec9b9fd8ba8c8591752');
-  Session.set('repTokenContractAddress', '0x02a2656ad55e07c3bc7b5d388e80d5a675b28a20');
-  Session.set('euroTokenContractAddress', '0x605832d1f474cafc26951287ec47d5c09334f1ce');
-  Session.set('priceFeedContractAddress', '0x81926CfbF418F7C5a9F024142818F6b7f75887F8 ');
-  Session.set('exchangeContractAddress', '0x50396a51a81b938ccb2d1466de9eebc49d5564f5');
-  Session.set('universeContractAddress', '0xa96e3e84603b0ed678fb612bcc686fb95a1228fb');
-  Session.set('versionContractAddress', '0x9d80c05f00a0d02d43c1fe8e64b5d2640430536b');
-  Session.set('governanceContractAddress', '0xdc181457ba55c5d7126155d5cdcd845d9ee0f6c3');
+  Session.set('etherTokenContractAddress', AddressList.EtherToken);
+  Session.set('melonTokenContractAddress', AddressList.MelonToken);
+  Session.set('bitcoinTokenContractAddress', AddressList.BitcoinToken);
+  Session.set('euroTokenContractAddress', AddressList.EuroToken);
+  Session.set('repTokenContractAddress', AddressList.RepToken);
+  Session.set('priceFeedContractAddress', AddressList.CryptoCompare);
+  Session.set('exchangeContractAddress', AddressList.Exchange);
+  Session.set('universeContractAddress', AddressList.Universe);
+  Session.set('versionContractAddress', AddressList.Version);
+  Session.set('governanceContractAddress', AddressList.Governance);
 
   Meteor.subscribe('cores');
   Meteor.subscribe('universes');
