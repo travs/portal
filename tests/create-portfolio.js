74e09b5ea3eac334c7cf6b831227631b50498a2e
Simon Emanuel Schmid <simon@oscillate.ch>
2017-05-01 18:01:24 +0200

    simple chimp setup

---
+++ tests/create-portfolio.js
@@ -0,0 +1,12 @@
+/* eslint-env mocha */
+/* globals browser */
+
+describe('list ui', () => {
+  beforeEach(function () {
+    browser.url('http://localhost:3000');
+  });
+
+  it('can create a list @watch', () => {
+    
+  });
+});
