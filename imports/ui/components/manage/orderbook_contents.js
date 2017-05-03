f9d0fd312eefaf1e6c63aa9ce4593e74848001fe
Simon Emanuel Schmid <simon@schmid.io>
2017-05-03 13:03:14 +0200

    calculate cumulative volume

--- imports/ui/components/manage/orderbook_contents.js
+++ imports/ui/components/manage/orderbook_contents.js
@@ -8,31 +8,51 @@ import specs from '/imports/lib/assets/utils/specs.js';
 // Corresponding html file
 import './orderbook_contents.html';
 
+
+const convertFromTokenPrecision = (value, precision) => {
+  const divisor = Math.pow(10, precision);
+  return value / divisor;
+};
+
 Template.orderbook_contents.onCreated(() => {
   Meteor.subscribe('orders');
+  console.log('Template.orderbook_contents.onCreated');
 });
 
 Template.orderbook_contents.helpers({
+  convertFromTokenPrecision,
+  more: false,
+  currentAssetPair: () => Session.get('currentAssetPair'),
+  quoteTokenSymbol: () => (Session.get('currentAssetPair') || '---/---').split('/')[0],
+  baseTokenSymbol: () => (Session.get('currentAssetPair') || '---/---').split('/')[1],
   orders() {
-    const [quoteTokenSymbol, baseTokenSymbol] = Session.get('currentAssetPair').split('/');
-    const quoteTokenAddress = specs.getTokenAddress(quoteTokenSymbol);
-    const baseTokenAddress = specs.getTokenAddress(baseTokenSymbol);
+    const [quoteTokenSymbol, baseTokenSymbol] = (Session.get('currentAssetPair') || '---/---').split('/');
 
-    return Orders.find({}, { sort: { id: -1 } });
+    return Orders.find({
+      isActive: true,
+      'buy.symbol': { $in: [quoteTokenSymbol, baseTokenSymbol] },
+      'sell.symbol': { $in: [quoteTokenSymbol, baseTokenSymbol] },
+    }, { sort: { 'buy.price': 1 } });
+  },
+  calcCumulativeVolume(buyPrice, precision) {
+    const [quoteTokenSymbol, baseTokenSymbol] = (Session.get('currentAssetPair') || '---/---').split('/');
+
+    const cheaperOrders = Orders.find({
+      isActive: true,
+      'buy.price': { $lte: buyPrice },
+      'buy.symbol': { $in: [quoteTokenSymbol, baseTokenSymbol] },
+      'sell.symbol': { $in: [quoteTokenSymbol, baseTokenSymbol] },
+    }, { sort: { 'buy.price': 1 } }).fetch();
+
+    const cummulatedDouble = cheaperOrders.reduce(
+      (accumulator, currentValue) => accumulator + currentValue.buy.howMuch, 0);
+
+    return convertFromTokenPrecision(cummulatedDouble, precision);
   },
   openOrders() {
     const address = FlowRouter.getParam('address');
     return Orders.find({ owner: address }, { sort: { id: -1 } });
   },
-  convertFromTokenPrecision(value) {
-    // TODO get precision from database
-    // if (Object.keys(this).length === 0) return '';
-    // const precision = this.precision;
-    const precision = 18;
-    const divisor = Math.pow(10, precision);
-    return value / divisor;
-  },
-
 });
 
 Template.orderbook_contents.onRendered(() => {
