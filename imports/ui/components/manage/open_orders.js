5316f625cddc07a6f4f827656e150ccc74f48c68
Simon Emanuel Schmid <simon@schmid.io>
2017-05-03 17:32:04 +0200

    first steps my open orders

--- imports/ui/components/manage/open_orders.js
+++ imports/ui/components/manage/open_orders.js
@@ -1,5 +1,10 @@
+// Meteor imports
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+// NPM imports
+import moment from 'moment';
+// Collections
+import { Orders } from '/imports/api/orders.js';
 
 // Corresponding html file
 import './open_orders.html';
@@ -7,6 +12,19 @@ import './open_orders.html';
 Template.open_orders.onCreated(() => {});
 
 Template.open_orders.helpers({
+  more: false,
+  currentAssetPair: () => Session.get('currentAssetPair'),
+  quoteTokenSymbol: () => (Session.get('currentAssetPair') || '---/---').split('/')[0],
+  baseTokenSymbol: () => (Session.get('currentAssetPair') || '---/---').split('/')[1],
+  getOpenOrders() {
+    const owner = '0x00e0b33cdb3af8b55cd8467d6d13bc0ba8035acf';
+
+    return Orders.find({
+      owner,
+      isActive: true,
+    });
+  },
+  formatDate: date => moment(date).format('D.M.YYYY HH:mm:ss'),
 });
 
 Template.open_orders.onRendered(() => {});
