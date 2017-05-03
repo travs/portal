013088ef4cca4d15fcbe3bf8978a234b0614713b
Simon Emanuel Schmid <simon@schmid.io>
2017-05-03 18:29:47 +0200

    my orders price

--- imports/ui/components/manage/open_orders.js
+++ imports/ui/components/manage/open_orders.js
@@ -3,6 +3,8 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 // NPM imports
 import moment from 'moment';
+// Utils
+import { convertFromTokenPrecision } from '/imports/lib/assets/utils/functions.js';
 // Collections
 import { Orders } from '/imports/api/orders.js';
 
@@ -17,12 +19,28 @@ Template.open_orders.helpers({
   quoteTokenSymbol: () => (Session.get('currentAssetPair') || '---/---').split('/')[0],
   baseTokenSymbol: () => (Session.get('currentAssetPair') || '---/---').split('/')[1],
   getOpenOrders() {
+    const [quoteTokenSymbol, baseTokenSymbol] = (Session.get('currentAssetPair') || '---/---').split('/');
     const owner = '0x00e0b33cdb3af8b55cd8467d6d13bc0ba8035acf';
 
     return Orders.find({
       owner,
       isActive: true,
+      $or: [
+        { 'buy.symbol': { $in: [quoteTokenSymbol, baseTokenSymbol] } },
+        { 'sell.symbol': { $in: [quoteTokenSymbol, baseTokenSymbol] } },
+      ],
     });
+  },
+  buyOrSell: buyTokenSymbol =>
+    (buyTokenSymbol === (Session.get('currentAssetPair') || '---/---').split('/')[1]
+    ? 'buy' : 'sell'
+  ),
+  getPrice(order) {
+    const baseTokenSymbol = (Session.get('currentAssetPair') || '---/---')[1];
+    const details = order.buy.symbol === baseTokenSymbol
+      ? order.buy : order.sell;
+
+    return details.price;
   },
   formatDate: date => moment(date).format('D.M.YYYY HH:mm:ss'),
 });
