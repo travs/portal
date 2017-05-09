113336aaf61fc5b021f27a1733c275b7bb78e61b
retotrinkler <rt@melonport.com>
2017-05-09 11:00:12 +0200

    Fix server-side web3 provider

--- imports/lib/server/ethereum/web3.js
+++ imports/lib/server/ethereum/web3.js
@@ -1,11 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 import Web3 from 'web3';
 
+const localNode = 'http://localhost:8547';
+const kovanNode = 'https://kovan.melonport.com';
+
 // set the provider you want from Web3.providers
-if ((new Web3(new Web3.providers.HttpProvider('http://localhost:8547'))).isConnected()) {
-  web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8547'));
-} else if ((new Web3(new Web3.providers.HttpProvider('https://kovan2.melonport.com'))).isConnected()) {
-  web3 = new Web3(new Web3.providers.HttpProvider('https://kovan.melonport.com'));
+if ((new Web3(new Web3.providers.HttpProvider(localNode))).isConnected()) {
+  web3 = new Web3(new Web3.providers.HttpProvider(localNode));
+} else if ((new Web3(new Web3.providers.HttpProvider(kovanNode))).isConnected()) {
+  web3 = new Web3(new Web3.providers.HttpProvider(kovanNode));
 } else {
   throw new Meteor.Error('No server-side web3 provider', 'Neither local instance nor node rpc connected');
 }
