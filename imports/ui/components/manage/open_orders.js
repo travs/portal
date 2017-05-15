638a0c28142664b8bd1fa72d0aec780b3876ddeb
Jenna Zenk <zenkjenna@gmail.com>
2017-05-15 12:00:16 +0200

    added cancel order functionnality in Open Orders

--- imports/ui/components/manage/open_orders.js
+++ imports/ui/components/manage/open_orders.js
@@ -4,12 +4,18 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 // NPM imports
 import moment from 'moment';
 // Utils
+import AddressList from '/imports/lib/ethereum/address_list.js';
 import { convertFromTokenPrecision } from '/imports/lib/assets/utils/functions.js';
 // Collections
 import { Orders } from '/imports/api/orders.js';
 
 // Corresponding html file
 import './open_orders.html';
+
+
+//Contracts
+import contract from 'truffle-contract';
+import CoreJson from '/imports/lib/assets/contracts/Core.json'; // Get Smart Contract JSON
 
 
 Template.open_orders.onCreated(() => {});
@@ -59,4 +65,19 @@ Template.open_orders.helpers({
 
 Template.open_orders.onRendered(() => {});
 
-Template.open_orders.events({});
+Template.open_orders.events({
+  'click .js-cancel': (event, order) => {
+    const Core = contract(CoreJson);
+    Core.setProvider(web3.currentProvider);
+    const coreAddress = FlowRouter.getParam('address');
+    const coreContract = Core.at(coreAddress);
+    const managerAddress = Session.get('clientManagerAccount');
+
+    coreContract.cancelOrder(AddressList.Exchange, event.currentTarget.dataset.id, { from: managerAddress }).then((result) => {
+      console.log(result);
+    })
+
+  }
+});
+
+
