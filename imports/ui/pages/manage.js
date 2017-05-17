0a322ae4a7929892a5bf77952b42fc49b3672e6a
Simon Emanuel Schmid <simon@schmid.io>
2017-05-17 11:43:36 +0200

    data preparation draft for depth of market visualisation

--- imports/ui/pages/manage.js
+++ imports/ui/pages/manage.js
@@ -10,20 +10,46 @@ import { Tracker } from 'meteor/tracker';
 // Collections
 import { Cores } from '/imports/api/cores';
 import { Orders } from '/imports/api/orders.js';
+// Utils
+import { convertFromTokenPrecision } from '/imports/lib/assets/utils/functions.js';
 // Components
-import '/imports/ui/components/manage/manage_overview.js';
 import '/imports/ui/components/manage/manage_holdings.js';
-import '/imports/ui/components/manage/orderbook_contents.js';
+import '/imports/ui/components/manage/manage_overview.js';
 import '/imports/ui/components/manage/open_orders.js';
+import '/imports/ui/components/manage/orderbook_contents.js';
 import '/imports/ui/components/manage/recent_trades.js';
 // Corresponding html file
 import './manage.html';
 
+
+const calcCumulativeVolume = (currentOrder) => {
+  let lowerThanCurrent = true;
+  return Orders.find({
+    isActive: true,
+    'buy.price': { $lte: currentOrder.buy.price },
+    'buy.symbol': currentOrder.buy.symbol,
+    'sell.symbol': currentOrder.sell.symbol,
+  }, {
+    sort: {
+      'buy.price': 1,
+      'buy.howMuch': 1,
+      createdAt: 1,
+    },
+  }).fetch()
+  .reduce((accumulator, currentValue) => {
+    const cumulativeVolume = lowerThanCurrent
+      ? accumulator + currentValue.buy.howMuch
+      : accumulator;
+
+    if (currentValue._id === currentOrder._id) { lowerThanCurrent = false; }
+
+    return cumulativeVolume;
+  }, 0);
+};
+
 Template.manage.onCreated(() => {});
 
-
 Template.manage.helpers({});
-
 
 Template.manage.onRendered(function () {
   $('select').select2();
@@ -51,31 +77,33 @@ Template.manage.onRendered(function () {
           createdAt: 1,
         },
       }).map(order => ({
-        price: order.buy.price,
-        howMuch: order.buy.howMuch,
-        symbol: order.buy.symbol,
+        buyPrice: order.buy.price,
+        buySymbol: order.buy.symbol,
+        buyHowMuch: convertFromTokenPrecision(order.buy.howMuch, order.buy.precision),
+        cumulativeVolume: convertFromTokenPrecision(
+          calcCumulativeVolume(order),
+          order.buy.precision,
+        ),
       }));
+
+      console.log(allOrders);
 
       // TODO: Read:
       // - https://www.tradingview.com/wiki/Depth_Of_Market_(DOM)
       // - https://tradeblock.com/blog/bitcoin-trading-interpreting-order-books/
-      console.log(allOrders);
 
       Meteor.defer(() => {
-        d3.json('data/fake_users1.json', (data) => {
-          data = MG.convert.date(data, 'date');
-          MG.data_graphic({
-            title: '',
-            description: 'Wallet Chart',
-            data,
-            full_width: true,
-            height: 250,
-            right: 40,
-            color: '#1189c6',
-            target: '#charts',
-            x_accessor: 'date',
-            y_accessor: 'value',
-          });
+        MG.data_graphic({
+          title: 'Depth Of Market',
+          chart_type: 'bar',
+          data: allOrders,
+          full_width: true,
+          height: 250,
+          right: 40,
+          color: '#1189c6',
+          target: '#charts',
+          x_accessor: 'buyPrice',
+          y_accessor: 'cumulativeVolume',
         });
       });
     }
