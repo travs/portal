812396f0aa669440ffab7f62bf65a3f266f0b730
Jenna Zenk <zenkjenna@gmail.com>
2017-05-17 18:27:26 +0200

    added ranking in Melon summary - it links to the leaderboard homepage

--- imports/ui/components/summary/summary_melon.js
+++ imports/ui/components/summary/summary_melon.js
@@ -4,12 +4,37 @@ import { Template } from 'meteor/templating';
 import '/imports/ui/components/ux/ux_spinner.js';
 // Corresponding html file
 import './summary_melon.html';
+// Collections
+import { Cores } from '/imports/api/cores';
 
 
 Template.summary_melon.onCreated(() => {});
 
 
-Template.summary_melon.helpers({});
+Template.summary_melon.helpers({
+  getRanking() {
+    const numberOfCores = Cores.find().count();
+    let coreAddress = FlowRouter.getParam('address');
+
+    if(!coreAddress && Cores.find({ owner: Session.get('clientManagerAccount') }).count() !== 0) {
+      coreAddress = Cores.findOne({ owner: Session.get('clientManagerAccount') }).address;
+    } else if(!coreAddress && Cores.find({ owner: Session.get('clientManagerAccount') }).count() == 0) {
+        return 'No ranking available.'
+    }
+
+    if(coreAddress) {
+      const sortedCores = Cores.find({}, { sort: { sharePrice: -1 } }).fetch();
+      let ranking;
+      for (let i = 0; i < sortedCores.length; i++) {
+        if (coreAddress == sortedCores[i]['address']) {
+          ranking = i + 1;
+          break;
+        }
+      }
+      return ranking + ' out of ' + numberOfCores;
+    }
+  },
+});
 
 
 Template.summary_melon.onRendered(() => {});
