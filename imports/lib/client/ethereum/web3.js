e41d5576051b5cb7c97e1159283ef104343a7a52
Simon Emanuel Schmid <simon@schmid.io>
2017-05-22 12:12:22 +0200

    poll web3 accounts and reload on change

--- imports/lib/client/ethereum/web3.js
+++ imports/lib/client/ethereum/web3.js
@@ -1,9 +1,19 @@
+/* global web3 */
+/* eslint-disable no-global-assign */
 import Web3 from 'web3';
 
 if (typeof web3 !== 'undefined') {
   web3 = new Web3(web3.currentProvider);
+
+  // HACK: check for account changes and reload the browser then
+  // - [ ] Improvement 1: Do not reload but update the state
+  // - [ ] Improvement 2: Push MetaMask to update their API
+  // (https://github.com/MetaMask/faq/blob/master/DEVELOPERS.md#ear-listening-for-selected-account-changes)
+  window.setInterval((initialAccount) => {
+    if (web3.eth.accounts[0] !== initialAccount) window.location.reload();
+  }, 500, web3.eth.accounts[0]);
 } else {
-  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 }
 
 export default web3;
