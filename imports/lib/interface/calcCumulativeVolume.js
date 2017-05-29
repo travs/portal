6b4eb6b24cba27accbebb5113d8a75a4b4e8bd47
Simon Emanuel Schmid <simon@schmid.io>
2017-05-29 17:53:49 +0200

    starting with calcCumulativeVolume

--- imports/lib/interface/calcCumulativeVolume.js
+++ imports/lib/interface/calcCumulativeVolume.js
@@ -1,7 +1,21 @@
+import BigNumber from 'bignumber.js';
 
 /*
-  @pre: orders are retrieved from the
+  @pre: orders are retrieved from the matchOrders (sorted, and filtered)
 */
-const calcCumulativeVolume = (id, orders) => {
+const calcCumulativeVolume = (orderType, orders) => {
+  if (orderType === 'buy') {
+    return orders.reduce((accumulator, current) =>
+      accumulator.add(current.buy.howMuchBigNumber)
+    , new BigNumber(0));
+  } else if (orderType === 'sell') {
+    return orders.reduce((accumulator, current) =>
+      accumulator.add(current.sell.howMuchBigNumber)
+    , new BigNumber(0));
+  }
 
-}
+  throw new Error('You need to specify orderType to be either "sell" or "buy"');
+};
+
+
+export default calcCumulativeVolume;
---
+++ imports/lib/interface/calcCumulativeVolume.test.js
@@ -0,0 +1,5 @@
+/* global test, expect */
+import calcCumulativeVolume from './calcCumulativeVolume';
+
+import orders from './fixtures/orderBook.js';
+
