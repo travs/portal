17f1d6336ef2bd48258ffd400bdc23e1b7ed1ddb
retotrinkler <rt@melonport.com>
2017-06-02 15:40:42 +0200

    Restructuring and cleanup

--- imports/ui/pages/portal.js
+++ imports/ui/pages/portal.js
@@ -1,12 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-import { FlowRouter } from 'meteor/kadira:flow-router';
-// Collections
-import { Cores } from '/imports/api/cores';
-// Components
 import '/imports/ui/components/portal/portalList';
 import '/imports/ui/components/portal/portalNew';
-// Corresponding html file
 import './portal.html';
 
 Template.portal.onCreated(() => {
@@ -18,7 +13,7 @@ Template.portal.helpers({});
 
 
 Template.portal.onRendered(() => {
-  Meteor.call('cores.sync'); // Upsert cores Collection
+  Meteor.call('cores.sync');
 });
 
 
