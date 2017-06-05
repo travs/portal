a963e9c3d818be2870920b329a3fad8bc199fbb9
Jenna Zenk <zenkjenna@gmail.com>
2017-06-05 12:07:31 +0200

    merge conflicts

--- imports/ui/pages/portal.js
+++ imports/ui/pages/portal.js
@@ -1,11 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-<<<<<<< HEAD
-// Collections
-import Cores from '/imports/api/cores';
-// Components
-=======
->>>>>>> 964f2c69dfbe19a999d037a9b5f6ead00ba2dc78
 import '/imports/ui/components/portal/portalList';
 import '/imports/ui/components/portal/portalNew';
 import './portal.html';
