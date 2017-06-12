841af52d4d4d4c1e1eb9c3045663077d95fda481
retotrinkler <rt@melonport.com>
2017-06-12 12:24:10 +0200

    Removing vault sync in portal and visit page

--- imports/ui/pages/visit.js
+++ imports/ui/pages/visit.js
@@ -1,17 +1,12 @@
-import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import '/imports/ui/components/portal/portalList';
 import './visit.html';
 
 
-Template.visit.onCreated(() => {
-  Meteor.subscribe('vaults');
-});
+Template.visit.onCreated(() => {});
 
 Template.visit.helpers({});
 
-Template.visit.onRendered(() => {
-  Meteor.call('vaults.sync');
-});
+Template.visit.onRendered(() => {});
 
 Template.visit.events({});
