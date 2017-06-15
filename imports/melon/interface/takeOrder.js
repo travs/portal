423a6baedffb6c836540731607eb5c35bb153510
Jenna Zenk <zenkjenna@gmail.com>
2017-06-15 11:34:54 +0200

    refactor test for takeOrder w async/await

--- imports/melon/interface/takeOrder.js
+++ imports/melon/interface/takeOrder.js
@@ -12,48 +12,39 @@ import getOrder from './getOrder';
 /*
   @param quantityAsked: BigNumber with Precision (i.e. '1.234' NOT '1234')
 */
-const takeOrder = (
+const takeOrder = async (
   id: number,
   managerAddress: string,
   coreAddress: string,
   quantityAsked: BigNumber,
-) =>
-  getOrder(id).then(async (rawOrder) => {
-    const Vault = contract(VaultJson);
-    const order = orderBigNumberify(rawOrder);
-    const sellHowMuchPrecise = order.sell.howMuchBigNumber;
+) => {
+  const rawOrder = await getOrder(id);
+  const Vault = contract(VaultJson);
+  const order = orderBigNumberify(rawOrder);
+  const sellHowMuchPrecise = order.sell.howMuchBigNumber;
 
-    const quantityWithPrecision =
-      !quantityAsked || quantityAsked.gte(sellHowMuchPrecise)
-      ? sellHowMuchPrecise
-      : quantityAsked;
+  const quantityWithPrecision =
+    !quantityAsked || quantityAsked.gte(sellHowMuchPrecise)
+    ? sellHowMuchPrecise
+    : quantityAsked;
 
-    const quantity = quantityWithPrecision.times(Math.pow(10, order.sell.precision));
+  const quantity = quantityWithPrecision.times(Math.pow(10, order.sell.precision));
 
-    Vault.setProvider(web3.currentProvider);
-    const coreContract = Vault.at(coreAddress);
+  Vault.setProvider(web3.currentProvider);
+  const coreContract = Vault.at(coreAddress);
 
-    if (!global.jest) {
-      console.log('taking order', order, {
-        exchange: addressList.exchange,
-        id: order.id,
-        quantity: quantity.toString(),
-        from: managerAddress,
-      });
-    }
+  const result = await coreContract.takeOrder(
+    addressList.exchange,
+    order.id,
+    quantity,
+    { from: managerAddress },
+  );
 
-    const result = await coreContract.takeOrder(
-      addressList.exchange,
-      order.id,
-      quantity,
-      { from: managerAddress },
-    );
-
-    return result ? {
-      executedQuantity: quantityWithPrecision,
-      result,
-    } : null;
-  });
+  return result ? {
+    executedQuantity: quantityWithPrecision,
+    result,
+  } : null;
+};
 
 
 export default takeOrder;
