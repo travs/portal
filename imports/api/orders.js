4a047522fbfb4965ef86ff51303869524d42f744
Simon Emanuel Schmid <simon@schmid.io>
2017-06-21 11:59:02 +0200

    serializeOrder

--- imports/api/orders.js
+++ imports/api/orders.js
@@ -8,12 +8,12 @@ import ExchangeJson from '@melonproject/protocol/build/contracts/Exchange.json';
 import web3 from '/imports/lib/web3';
 import addressList from '/imports/melon/interface/addressList';
 import getOrder from '/imports/melon/interface/getOrder';
-
+import serializeOrder from '/imports/melon/interface/helpers/serializeOrder';
 
 const NUMBERS_OF_ORDERS_TO_SYNC_ON_STARTUP = 96;
 
 // COLLECTIONS
-const Orders = global.Orders = new Mongo.Collection('orders');
+const Orders = (global.Orders = new Mongo.Collection('orders'));
 
 if (Meteor.isServer) {
   // Note: you need to specify an asset pair. There is no way to get all orders to the client.
@@ -21,11 +21,14 @@ if (Meteor.isServer) {
     check(currentAssetPair, String);
     const [baseTokenSymbol, quoteTokenSymbol] = currentAssetPair.split('/');
 
-    return Orders.find({
-      isActive: true,
-      'buy.symbol': { $in: [baseTokenSymbol, quoteTokenSymbol] },
-      'sell.symbol': { $in: [baseTokenSymbol, quoteTokenSymbol] },
-    }, { sort: { id: -1 } });
+    return Orders.find(
+      {
+        isActive: true,
+        'buy.symbol': { $in: [baseTokenSymbol, quoteTokenSymbol] },
+        'sell.symbol': { $in: [baseTokenSymbol, quoteTokenSymbol] },
+      },
+      { sort: { id: -1 } },
+    );
   });
 }
 
@@ -36,18 +39,23 @@ Orders.watch = () => {
   Exchange.setProvider(web3.currentProvider);
   const exchangeContract = Exchange.at(addressList.exchange); // Initialize contract instance
 
-  const orders = exchangeContract.OrderUpdate({}, {
-    fromBlock: web3.eth.blockNumber,
-    toBlock: 'latest',
-  });
+  const orders = exchangeContract.OrderUpdate(
+    {},
+    {
+      fromBlock: web3.eth.blockNumber,
+      toBlock: 'latest',
+    },
+  );
 
-  orders.watch(Meteor.bindEnvironment((err, event) => {
-    if (err) throw err;
+  orders.watch(
+    Meteor.bindEnvironment((err, event) => {
+      if (err) throw err;
 
-    console.log('Order updated', event.args.id.toNumber());
+      console.log('Order updated', event.args.id.toNumber());
 
-    Orders.syncOrderById(event.args.id.toNumber());
-  }));
+      Orders.syncOrderById(event.args.id.toNumber());
+    }),
+  );
 };
 
 Orders.sync = () => {
@@ -56,7 +64,9 @@ Orders.sync = () => {
   const exchangeContract = Exchange.at(addressList.exchange);
 
   exchangeContract.getLastOrderId().then((lastId) => {
-    const endIndex = lastId.minus(NUMBERS_OF_ORDERS_TO_SYNC_ON_STARTUP).toNumber();
+    const endIndex = lastId
+      .minus(NUMBERS_OF_ORDERS_TO_SYNC_ON_STARTUP)
+      .toNumber();
 
     for (let id = lastId.toNumber(); id > endIndex; id -= 1) {
       Orders.syncOrderById(id);
@@ -65,25 +75,29 @@ Orders.sync = () => {
 };
 
 Orders.syncOrderById = (id) => {
-  getOrder(id).then((order) => {
-    if (order.sell.token !== '0x0000000000000000000000000000000000000000') {
-      console.log('syncOrder with DB', order.id);
-    }
+  getOrder(id)
+    .then((order) => {
+      if (order.sell.token !== '0x0000000000000000000000000000000000000000') {
+        console.log('syncOrder with DB', order.id);
+      }
 
-    if (order.isActive) {
-      Orders.upsert({
-        id: order.id,
-      }, {
-        ...order,
-        createdAt: new Date(),
-      });
-    } else {
-      Orders.remove({ id: order.id });
-    }
-  })
-  .catch((err) => {
-    throw err;
-  });
+      if (order.isActive) {
+        Orders.upsert(
+          {
+            id: order.id,
+          },
+          {
+            ...serializeOrder(order),
+            createdAt: new Date(),
+          },
+        );
+      } else {
+        Orders.remove({ id: order.id });
+      }
+    })
+    .catch((err) => {
+      throw err;
+    });
 };
 
 // METEOR METHODS
@@ -98,6 +112,5 @@ Meteor.methods({
     if (Meteor.isServer) Orders.syncOrderById(id);
   },
 });
-
 
 export default Orders;
