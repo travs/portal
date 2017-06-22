4a122dd6b9b238293c1a931c106f53891e6a263d
Jenna Zenk <zenkjenna@gmail.com>
2017-06-22 12:24:31 +0200

    cumulative volume for buy and sell side on orderbook refactored

--- imports/ui/components/manage/orderBookContents.js
+++ imports/ui/components/manage/orderBookContents.js
@@ -10,6 +10,8 @@ import cumulativeVolume from '/imports/melon/interface/cumulativeVolume';
 import matchOrders from '/imports/melon/interface/matchOrders';
 import getOrders from '/imports/melon/interface/getOrders';
 import getPrices from '/imports/melon/interface/helpers/getPrices';
+import filterByAssetPair from '/imports/melon/interface/query/filterByAssetPair';
+import sortByPrice from '/imports/melon/interface/query/sortByPrice';
 
 // Corresponding html file
 import './orderBookContents.html';
@@ -34,128 +36,23 @@ Template.orderBookContents.helpers({
       'currentAssetPair',
     ) || '---/---')
       .split('/');
-    const liquidityProviderOrders = Orders.find(
-      {
-        isActive: true,
-        'buy.symbol': baseTokenSymbol,
-        'sell.symbol': quoteTokenSymbol,
-        owner: addressList.liquidityProvider,
-      },
-      { sort: { 'buy.price': -1, 'buy.howMuch': 1, createdAt: 1 } },
-    );
-    const allOrders = Orders.find(
-      {
-        isActive: true,
-        'buy.symbol': baseTokenSymbol,
-        'sell.symbol': quoteTokenSymbol,
-      },
-      { sort: { 'buy.price': -1, 'buy.howMuch': 1, createdAt: 1 } },
-    );
-    if (Session.get('fromPortfolio')) return liquidityProviderOrders;
-    else if (!Session.get('fromPortfolio')) return allOrders;
+    return Orders.find(filterByAssetPair(baseTokenSymbol, quoteTokenSymbol, 'buy', true), { sort: sortByPrice('buy') });
   },
-  calcBuyPrice: order => getPrices(order).buy.toFixed(4),
-  calcSellPrice: order => getPrices(order).sell.toFixed(4),
-  displayVolume: (howMuch, dec) => howMuch.toFixed(dec),
   sellOrders() {
     const [baseTokenSymbol, quoteTokenSymbol] = (Session.get(
       'currentAssetPair',
     ) || '---/---')
       .split('/');
-    const liquidityProviderOrders = Orders.find(
-      {
-        isActive: true,
-        'buy.symbol': quoteTokenSymbol,
-        'sell.symbol': baseTokenSymbol,
-        owner: addressList.liquidityProvider,
-      },
-      { sort: { 'sell.price': 1, 'buy.howMuch': 1, createdAt: 1 } },
-    );
-    const allOrders = Orders.find(
-      {
-        isActive: true,
-        'buy.symbol': quoteTokenSymbol,
-        'sell.symbol': baseTokenSymbol,
-      },
-      { sort: { 'sell.price': 1, 'buy.howMuch': 1, createdAt: 1 } },
-    );
-
-    if (Session.get('fromPortfolio')) return liquidityProviderOrders;
-    else if (!Session.get('fromPortfolio')) return allOrders;
+    return Orders.find(filterByAssetPair(baseTokenSymbol, quoteTokenSymbol, 'sell', true), { sort: sortByPrice('sell') });
   },
-  calcBuyCumulativeVolume(buyPrice, precision, index) {
-    const [baseTokenSymbol, quoteTokenSymbol] = (Session.get(
-      'currentAssetPair',
-    ) || '---/---')
-      .split('/');
-    let cheaperOrders;
-
-    if (Session.get('fromPortfolio')) {
-      cheaperOrders = Orders.find(
-        {
-          isActive: true,
-          'buy.price': { $gte: buyPrice },
-          'buy.symbol': baseTokenSymbol,
-          'sell.symbol': quoteTokenSymbol,
-          owner: addressList.liquidityProvider,
-        },
-        { sort: { 'buy.price': -1, 'buy.howMuch': 1, createdAt: 1 } },
-      ).fetch();
-    } else {
-      cheaperOrders = Orders.find(
-        {
-          isActive: true,
-          'buy.price': { $gte: buyPrice },
-          'buy.symbol': baseTokenSymbol,
-          'sell.symbol': quoteTokenSymbol,
-        },
-        { sort: { 'buy.price': -1, 'buy.howMuch': 1, createdAt: 1 } },
-      ).fetch();
-    }
-    let cumulativeDouble = 0;
-
-    for (let i = 0; i <= index; i += 1) {
-      cumulativeDouble += cheaperOrders[i].buy.howMuch;
-    }
-
-    return convertFromTokenPrecision(cumulativeDouble, precision);
-  },
-  calcSellCumulativeVolume(sellPrice, precision, index) {
-    const [baseTokenSymbol, quoteTokenSymbol] = (Session.get(
-      'currentAssetPair',
-    ) || '---/---')
-      .split('/');
-    let cheaperOrders;
-    if (Session.get('fromPortfolio')) {
-      cheaperOrders = Orders.find(
-        {
-          isActive: true,
-          'sell.price': { $lte: sellPrice },
-          'sell.symbol': baseTokenSymbol,
-          'buy.symbol': quoteTokenSymbol,
-          owner: addressList.liquidityProvider,
-        },
-        { sort: { 'sell.price': 1, 'buy.howMuch': 1, createdAt: 1 } },
-      ).fetch();
-    } else {
-      cheaperOrders = Orders.find(
-        {
-          isActive: true,
-          'sell.price': { $lte: sellPrice },
-          'sell.symbol': baseTokenSymbol,
-          'buy.symbol': quoteTokenSymbol,
-        },
-        { sort: { 'sell.price': 1, 'buy.howMuch': 1, createdAt: 1 } },
-      ).fetch();
-    }
-
-    let cumulativeDouble = 0;
-
-    for (let i = 0; i <= index; i += 1) {
-      cumulativeDouble += cheaperOrders[i].sell.howMuch;
-    }
-
-    return convertFromTokenPrecision(cumulativeDouble, precision);
+  calcBuyPrice: order => getPrices(order).buy.toFixed(4),
+  calcSellPrice: order => getPrices(order).sell.toFixed(4),
+  displayVolume: (howMuch, dec) => howMuch.toFixed(dec),
+  cumulativeVolume(order, orderType) {
+    const orders = getOrders(orderType, Session.get('currentAssetPair'));
+    const priceThreshold = getPrices(order)[orderType];
+    const matchedOrders = matchOrders(orderType, priceThreshold, orders);
+    return cumulativeVolume(orderType, matchedOrders).toNumber().toFixed(4);
   },
   percentageOfBuySum(buyPrice, precision, index) {
     const currentCumVol = Template.orderBookContents.__helpers
@@ -205,12 +102,6 @@ Template.orderBookContents.helpers({
       );
 
     return currentCumVol / convertFromTokenPrecision(total, precision) * 100;
-  },
-  cumulativeVolume(order, orderType) {
-    const orders = getOrders(orderType, Session.get('currentAssetPair'));
-    const priceThreshold = getPrices(order)[orderType];
-    const matchedOrders = matchOrders(orderType, priceThreshold, orders);
-    return cumulativeVolume(orderType, matchedOrders).toNumber();
   },
 
 });
