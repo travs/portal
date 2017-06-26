5465b36bc1f9c5b60dbc3755dea6d0abb913b5e0
Jenna Zenk <zenkjenna@gmail.com>
2017-06-26 17:54:11 +0200

    label core -> vault

--- imports/ui/components/portfolio/manageParticipation.js
+++ imports/ui/components/portfolio/manageParticipation.js
@@ -56,19 +56,31 @@ Template.manageParticipation.onRendered(() => {
 
 Template.manageParticipation.events({
   'change select#type': (event, templateInstance) => {
-    const currentlySelectedTypeValue = parseFloat(templateInstance.find('select#type').value, 10);
+    const currentlySelectedTypeValue = parseFloat(
+      templateInstance.find('select#type').value,
+      10,
+    );
     Template.instance().typeValue.set(currentlySelectedTypeValue);
   },
   'input input#price': (event, templateInstance) => {
     const price = parseFloat(templateInstance.find('input#price').value, 10);
     const volume = parseFloat(templateInstance.find('input#volume').value, 10);
     const total = parseFloat(templateInstance.find('input#total').value, 10);
-    if (!isNaN(volume)) templateInstance.find('input#total').value = price * volume;
-    else if (!isNaN(total)) templateInstance.find('input#volume').value = total / price;
+    if (!isNaN(volume)) {
+      templateInstance.find('input#total').value = price * volume;
+    } else if (!isNaN(total)) {
+      templateInstance.find('input#volume').value = total / price;
+    }
   },
   'input input#volume': (event, templateInstance) => {
-    const price = parseFloat(templateInstance.find('input#price').value || 0, 10);
-    const volume = parseFloat(templateInstance.find('input#volume').value || 0, 10);
+    const price = parseFloat(
+      templateInstance.find('input#price').value || 0,
+      10,
+    );
+    const volume = parseFloat(
+      templateInstance.find('input#volume').value || 0,
+      10,
+    );
     console.log('price: ', price, 'volume: ', volume);
     /* eslint no-param-reassign: ["error", { "props": false }]*/
     templateInstance.find('input#total').value = price * volume;
@@ -101,16 +113,16 @@ Template.manageParticipation.events({
       // Materialize.toast('Not connected, use Parity, Mist or MetaMask', 4000, 'blue');
       return;
     }
-    const coreAddress = FlowRouter.getParam('address');
-    const doc = Vaults.findOne({ address: coreAddress });
+    const vaultAddress = FlowRouter.getParam('address');
+    const doc = Vaults.findOne({ address: vaultAddress });
     // Check if core is stored in database
     if (doc === undefined) {
       // TODO replace toast
-      // Materialize.toast(`Portfolio could not be found\n ${coreAddress}`, 4000, 'red');
+      // Materialize.toast(`Portfolio could not be found\n ${vaultAddress}`, 4000, 'red');
       return;
     }
 
-    const coreContract = Vault.at(coreAddress);
+    const vaultContract = Vault.at(vaultAddress);
     Vault.setProvider(web3.currentProvider);
 
     // Is mining
@@ -135,9 +147,13 @@ Template.manageParticipation.events({
       case 0:
         EtherTokenContract.deposit({ from: managerAddress, value: weiTotal })
           .then(result =>
-            EtherTokenContract.approve(coreAddress, baseUnitVolume, { from: managerAddress }),
+            EtherTokenContract.approve(vaultAddress, baseUnitVolume, {
+              from: managerAddress,
+            }),
           )
-          .then(result => coreContract.createShares(baseUnitVolume, { from: managerAddress }))
+          .then(result =>
+            vaultContract.createShares(baseUnitVolume, { from: managerAddress }),
+          )
           .then((result) => {
             Session.set('NetworkStatus', {
               isInactive: false,
@@ -147,9 +163,9 @@ Template.manageParticipation.events({
             });
             toastr.success('Shares successfully created!');
             console.log(`Shares successfully created. Tx Hash: ${result}`);
-            Meteor.call('assets.sync', coreAddress); // Upsert Assets Collection
+            Meteor.call('assets.sync', vaultAddress); // Upsert Assets Collection
             Meteor.call('vaults.syncVaultById', doc.id);
-            return coreContract.totalSupply();
+            return vaultContract.totalSupply();
           })
           .catch((error) => {
             console.log(error);
@@ -170,7 +186,7 @@ Template.manageParticipation.events({
 
       // Redeem case
       case 1:
-        coreContract
+        vaultContract
           .annihilateShares(baseUnitVolume, weiTotal, { from: managerAddress })
           .then((result) => {
             Session.set('NetworkStatus', {
@@ -181,10 +197,10 @@ Template.manageParticipation.events({
             });
             toastr.success('Shares successfully redeemed!');
             console.log(`Shares annihilated successfully. Tx Hash: ${result}`);
-            Meteor.call('assets.sync', coreAddress); // Upsert Assets Collection
+            Meteor.call('assets.sync', vaultAddress); // Upsert Assets Collection
             templateInstance.find('input#total').value = '';
             templateInstance.find('input#volume').value = '';
-            return coreContract.totalSupply();
+            return vaultContract.totalSupply();
           })
           .catch((error) => {
             console.log(error);
